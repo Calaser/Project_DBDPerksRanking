95ef262b9f204750e4d74bf42794e8c906a889c8
Calaser <s33003030@gmail.com>
2023-07-17 06:13:33 +0800

    CompleteBasicTierListFunction
    0.collect lost part of  survivor perks icons and rename it to fit perks information document
    1.Complete Basic function of Fetch perks description and tier list data
    2.create perk DOM with add Event Listener which show perk description, place all of perks to tier list

--- script.js
+++ script.js
@@ -1,45 +1,90 @@
-// const btn = document.getElementById('btn');
-// const menu = document.getElementById('wrapper');
+let perksRanking;
+fetch("perks_ranking.json")
+   .then(res => res.json())
+   .then(json => perksRanking = json);
 
-// btn.addEventListener('click', () => {
-//     btn.classList.toggle('trigger');
-//     menu.classList.toggle('trigger');
-// });
-
-
-// var xhr = new XMLHttpRequest();
-// xhr.open('GET', 'perks_info.json', true);
-// xhr.onreadystatechange = function() {
-//   if (xhr.readyState === 4 && xhr.status === 200) {
-//     var data = JSON.parse(xhr.responseText);
-//     console.log(data);
-//   }
-// };
-// xhr.send();
-
-
-// 在終端使用 live-server 指令以開啟local host
-const content = document.getElementById("content");
-const json = [];
 fetch("perks_info.json")
    .then(res => res.json())
    .then(json => {
-      console.log(json.Adrenaline);
-      content.innerHTML = `
-      <div class="perk_info">
-         <img src=${json.Adrenaline.image}></img>
-         <h3>${json.Adrenaline.name}</h3>
-         <p>${(function fun() {
-            desc = json.Adrenaline.description;
-            for(var i = 0; i < json.Adrenaline.variable.length; i++) {
-               desc = desc.replace(`{${i}}`, json.Adrenaline.variable[i].slice(-1));
+      const keys = Object.keys(json);
+      keys.forEach(key => {
+         if(json[key].role === "survivor") {
+         //create perk DOM
+         const createIcon = document.createElement("img");
+         createIcon.id = json[key].name;
+         createIcon.src = `img/IconPerks_${json[key].image.slice(json[key].image.indexOf("_") + 1, json[key].image.indexOf("."))}.webp`;
+
+         //place perk DOM to tier list
+         for (let i = 0; i < 5; i++) {
+            for (let j = 0; j < perksRanking[i].length; j++) {
+               if (perksRanking[i][j] === json[key].name) {
+                  console.log(`${perksRanking[i][j]} in perksRanking[${i}][${j}]`);
+                  createIcon.style.order = j;
+                  document.getElementById(`star${5 - i}`).appendChild(createIcon);
+               }
             }
-            return desc;
-         })()}
-         </p>
-      </div>
-   `;
+         }
+
+         //create call perk description EventListener
+         const content = document.getElementById("content");
+         createIcon.addEventListener('click', () => {
+            content.innerHTML = `
+            <div class="perk_info">
+               <img src="img/IconPerks_${json[key].image.slice(json[key].image.lastIndexOf("_") + 1, json[key].image.lastIndexOf("."))}.webp"></img>
+               <h2>${json[key].name}</h2>
+   
+               <p>${(function fun() {
+                  desc = json[key].description;
+                  for (var j = 0; j < json[key].tunables.length; j++) {
+                     desc = desc.replace(`{${j}}`, json[key].tunables[j].slice(-1));
+                  }
+                  return desc;
+               })()}
+               </p>
+            </div>
+            `;
+         })
+      }
+      })
    })
    .catch(error => {
       console.log("Error:", error);
    });
+
+
+
+
+
+
+
+
+
+//    document.getElementById(key[i]).addEventListener('click', () => {
+//       const content = document.getElementById("content");
+//       fetch("perks_info.json")
+//          .then(res => res.json())
+//          .then(json => {
+//             console.log(json.key[i]);
+//             content.innerHTML = `
+//          <div class="perk_info">
+//             <img src="img/IconPerks_${json.key[i].name}.webp"></img>
+//             <h2>${json.key[i].name}</h2>
+//             <p>${(function fun() {
+//                   desc = json.key[i].description;
+//                   for (var j = 0; j < json.key[j].variable.length; j++) {
+//                      desc = desc.replace(`{${j}}`, json.key[j].variable[j].slice(-1));
+//                   }
+//                   return desc;
+//                })()}
+//             </p>
+//          </div>
+//          `;
+//          })
+//          .catch(error => {
+//             console.log("Error:", error);
+//          });
+//    });
+// }
+
+
+// 在終端使用 live-server 指令以開啟local host
