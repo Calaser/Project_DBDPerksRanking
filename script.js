ca3e5d2717cd2d2fd044dc0933ecd6bfa88d9ddb
Calaser <s33003030@gmail.com>
2023-07-30 06:01:20 +0800

    Layout change
    1.Change layout in small screen, now the whole perk info section will be modal style in small resolution.
    2.Add review section and import review doc function.

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
-let perksRanking;
+let perksReview;
+fetch("perks_review.json")
+   .then(res => res.json())
+   .then(json => perksReview = json)
+   .catch(error => {
+      console.log("Perks Review Fetch Error!", error);
+   })
+
+
+let perksRankingData;
 fetch("perks_ranking.json")
    .then(res => res.json())
-   .then(json => perksRanking = json);
+   .then(json => perksRankingData = json)
+   .catch(error => {
+      console.log("Perks Ranking Fetch Error!", error);
+   })
 
+let perksInfoData;
 fetch("perks_info.json")
    .then(res => res.json())
+   .then(json => perksInfoData = json)
    .then(json => {
-      const keys = Object.keys(json);
-      keys.forEach(key => {
-         if(json[key].role === "survivor") {
+      perksInfoData = json;
+
+      renderFunction(perksRankingData, perksInfoData);
+   })
+   .catch(error => {
+      console.log("Perks Info Fetch / Render Error!", error);
+   });
+
+
+function renderFunction(perksRankingData, perksInfoData) {
+   const keys = Object.keys(perksInfoData);
+   keys.forEach(key => {
+      if (perksInfoData[key].role === "survivor") {
          //create perk DOM
          const createIcon = document.createElement("img");
-         createIcon.id = json[key].name;
-         createIcon.src = `img/IconPerks_${json[key].image.slice(json[key].image.indexOf("_") + 1, json[key].image.indexOf("."))}.webp`;
+         createIcon.id = perksInfoData[key].name;
+         createIcon.src = `img/IconPerks_${perksInfoData[key].image.slice(perksInfoData[key].image.indexOf("_") + 1, perksInfoData[key].image.indexOf("."))}.webp`;
 
          //place perk DOM to tier list
          for (let i = 0; i < 5; i++) {
-            for (let j = 0; j < perksRanking[i].length; j++) {
-               if (perksRanking[i][j] === json[key].name) {
-                  console.log(`${perksRanking[i][j]} in perksRanking[${i}][${j}]`);
+            for (let j = 0; j < perksRankingData[i].length; j++) {
+               if (perksRankingData[i][j] === perksInfoData[key].name) {
                   createIcon.style.order = j;
                   document.getElementById(`star${5 - i}`).appendChild(createIcon);
                }
@@ -27,64 +50,47 @@ fetch("perks_info.json")
 
          //create call perk description EventListener
          const content = document.getElementById("content");
+         const contentWrapper = document.getElementById("contentWrapper");
+         const contentBackground = document.getElementById("content_background");
          createIcon.addEventListener('click', () => {
+            contentWrapper.classList.add("show");
+            contentBackground.classList.add("show");
             content.innerHTML = `
-            <div class="perk_info">
-               <img src="img/IconPerks_${json[key].image.slice(json[key].image.lastIndexOf("_") + 1, json[key].image.lastIndexOf("."))}.webp"></img>
-               <h2>${json[key].name}</h2>
+            <div id="perk_info">
+               <img src="img/IconPerks_${perksInfoData[key].image.slice(perksInfoData[key].image.lastIndexOf("_") + 1, perksInfoData[key].image.lastIndexOf("."))}.webp"></img>
+               <h2>${perksInfoData[key].name}</h2>
    
-               <p>${(function fun() {
-                  desc = json[key].description;
-                  for (var j = 0; j < json[key].tunables.length; j++) {
-                     desc = desc.replace(`{${j}}`, json[key].tunables[j].slice(-1));
+               <p>${(function fun() { //use IIFE for looping
+                  desc = perksInfoData[key].description;
+                  for (var j = 0; j < perksInfoData[key].tunables.length; j++) {
+                     desc = desc.replace(`{${j}}`, perksInfoData[key].tunables[j].slice(-1));
                   }
                   return desc;
                })()}
                </p>
             </div>
+
+            <div id="perk_review">
+               <h2>Review</h2>
+               <p>
+                  ${perksReview[perksInfoData[key].name] || "No comment."}
+               </p>
+            </div>
+
+            <div id="content_closeBtn">✖</div>
             `;
+            const closeBtn = document.getElementById("content_closeBtn");
+            closeBtn.addEventListener('click', () => {
+               contentWrapper.classList.remove("show");
+               contentBackground.classList.remove("show");
+            })
+            contentBackground.addEventListener('click', () => {
+               contentWrapper.classList.remove("show");
+               contentBackground.classList.remove("show");
+            })
          })
       }
-      })
    })
-   .catch(error => {
-      console.log("Error:", error);
-   });
-
-
-
-
-
-
-
-
-
-//    document.getElementById(key[i]).addEventListener('click', () => {
-//       const content = document.getElementById("content");
-//       fetch("perks_info.json")
-//          .then(res => res.json())
-//          .then(json => {
-//             console.log(json.key[i]);
-//             content.innerHTML = `
-//          <div class="perk_info">
-//             <img src="img/IconPerks_${json.key[i].name}.webp"></img>
-//             <h2>${json.key[i].name}</h2>
-//             <p>${(function fun() {
-//                   desc = json.key[i].description;
-//                   for (var j = 0; j < json.key[j].variable.length; j++) {
-//                      desc = desc.replace(`{${j}}`, json.key[j].variable[j].slice(-1));
-//                   }
-//                   return desc;
-//                })()}
-//             </p>
-//          </div>
-//          `;
-//          })
-//          .catch(error => {
-//             console.log("Error:", error);
-//          });
-//    });
-// }
-
+}
 
 // 在終端使用 live-server 指令以開啟local host
